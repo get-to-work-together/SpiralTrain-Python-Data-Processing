c3449d1e4d8b0a5ded331705e3bdd293f1abfbd4
Peter Anema <email@peteranema.nl>
2022-06-20 12:44:25 +0200

    Update

--- Sandbox/hello.py
+++ Sandbox/hello.py
@@ -1,42 +1,90 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 23 10:37:16 2022
+# This is comment
 
-@author: peter
+name = 'Peter'
 
-This is a dcostring with a lot of information
-"""
-
-#%% hello
-# This is a comment
-# Good to do
-# Only why not how!
-
-print('Hello GustoMSC')
+print('Hello ' + name)
 
 #%%
-#
-print('Next cell')
 
-#%% calculate
+print('This is the second cell')
 
-result = (12 + 89) * 65
-print(result)
+#%%
 
-#%% Hello name
+print('This is the third cell')
+
+
+#%%
 
 name = input('What is your name? ')
 
-name = "Peter"
-
-print('Hello ' + name + '!')
+print('Hello ' + name)
 
 #%%
-def my_function():
-    """This is my very own function!!!!
-    """
-    print('Hi')
-    
-my_function()
+
+# PEP8 - coding style
+
+number_of_cars = 6              # YES - snake case
+
+n = 9                           # NO
+
+numbersOfCars = 9               # NO - camelCase
+
+NumberOfCars = 9                # Only for class names - Pascal case
+
+intNumberOfCars = 9
+
+NUMBER_OF_CARS = 9              # Only for constants - SCREAMING_CASE
+
+#%%
+
+import sys
+
+print(sys.name)
+
+#%%
+
+from math import pi
+
+print(pi)
+
+
+#%% Docstring
+
+"""This is documentation. Using triple quotes.
+
+Multiple lines are OK!"""
+
+#%% Keywords
+
+#%% Numeric
+
+number = 320007987878678968768768768768768768768768768768768767867868
+print(type(number))
+
+print(number + 1)
+
+
+x1 = 0.1
+x2 = 0.2
+
+print(x1 + x1)
+print(x2 + x2)
+print(x1 + x2)
+
+print(x1 + x2 == 0.3)
+
+#%% Decimal
+
+from decimal import Decimal
+
+d1 = Decimal('0.1')
+d2 = Decimal('0.2')
+
+d3 = d1 + d2
+
+print(d3)
+
+
+
 
 
---
+++ Sandbox/sys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 20 11:18:05 2022
+
+@author: peter
+"""
+
+name = 'XXXXX9.0PLUS'
